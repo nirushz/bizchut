74a9bb097cc7c7d0e877a05984a4898a41746259
nirushz <nirushz@gmail.com>
2018-07-16 00:09:22 +0300

    vilon animation

--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,25 @@
-let screenHeight = screen.height;
-document.getElementById("loader").style.height = screenHeight + "px";
+
+let mainContainer = document.getElementById("main-container");
+let loader = document.getElementById("loader");
+loader.addEventListener("animationstart", loaderAnimationStart, false);
+//loader.addEventListener("animationend", ()=>{console.log("animationend")}, false);
+
+let shouldLoadDataToday = true; //TODO: check local storage for today data
+
+
+if (shouldLoadDataToday){
+    mainContainer.style.display = "none"
+    loader.style.height = screen.height + "px";
+}
+
+setTimeout(loadDataCompleted, 2000);
+
+function loadDataCompleted(){
+    console.log("loadDataCompleted");
+    loader.className +=" slide-down"
+    document.getElementById("logo-background-img").className = "fadeOut";
+}
+
+function loaderAnimationStart(){
+    mainContainer.style.display = "block"
+}
