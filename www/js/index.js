5c17fd43d935df1f7ac7829a517517169bf037f4
nirushz <nirushz@gmail.com>
2018-07-22 13:45:13 +0300

    fetch api

--- www/js/index.js
+++ www/js/index.js
@@ -2,20 +2,32 @@
 let mainContainer = document.getElementById("main-container");
 let loader = document.getElementById("loader");
 loader.addEventListener("animationstart", loaderAnimationStart, false);
-//loader.addEventListener("animationend", ()=>{console.log("animationend")}, false);
+
 
 let shouldLoadDataToday = true; //TODO: check local storage for today data
-
+let postsData;
 
 if (shouldLoadDataToday){
     mainContainer.style.display = "none"
     loader.style.height = screen.height + "px";
+    let categoriesToFetch = [2,3,4];
+    fetchData(categoriesToFetch)
+
 }
+
+
+async function fetchData (categoriesToFetch) {
+    // await response of fetch call
+    let response = await fetch(`https://bizchut-nashim.com/wp-json/wp/v2/posts/?categories=${categoriesToFetch}`);
+    // only proceed once promise is resolved
+    postsData = await response.json();
+}
+
 
 setTimeout(loadDataCompleted, 2000);
 
 function loadDataCompleted(){
-    console.log("loadDataCompleted");
+    console.log(postsData);
     loader.className +=" slide-down fadeOut"
     //document.getElementById("loader").className = "fadeOut";
     localStorage.setItem("loadDataCompleted-time", new Date().getTime().toString())
